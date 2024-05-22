179f340b4843c983021eb683f7ef5cc948da5ff0
WooJJam <jaemin5548@naver.com>
2024-05-22 15:23:13 +0900

    :heavy_plus_sign: json-simple, Starter Web 의존성 추가 #1

--- src/main/java/CodeIt/Ytrip/auth/controller/AuthController.java
+++ src/main/java/CodeIt/Ytrip/auth/controller/AuthController.java
@@ -4,7 +4,10 @@ import CodeIt.Ytrip.auth.dto.response.KakaoLoginResponse;
 import CodeIt.Ytrip.auth.service.AuthService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Map;
 
