1d2a256751aa0edcb62c23c2f438411be6c114f7
WooJJam <jaemin5548@naver.com>
2024-05-23 20:02:19 +0900

    :white_check_mark: JWT 토큰 생성 및 복호화 테스트 코드 수정

--- src/test/java/CodeIt/Ytrip/common/JwtUtilsTest.java
+++ src/test/java/CodeIt/Ytrip/common/JwtUtilsTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
 
 @SpringBootTest
 class JwtUtilsTest {
@@ -16,18 +16,16 @@ class JwtUtilsTest {
     
     @Test
     public void 토큰_생성() throws Exception {
-        String s = jwtUtils.generateToken("1");
+        String s = jwtUtils.generateToken(10L, 1000 * 60 * 60, "AccessToken");
         Assertions.assertNotNull(s);
         System.out.println("s = " + s);
     }
     
     @Test
     public void 토큰_복호화() {
-        String userId = "UTrip";
-        String token = jwtUtils.generateToken("jaemin5548");
-        System.out.println("token = " + token);
+        String token = jwtUtils.generateToken(10L, 1000 * 60 * 60, "AccessToken");
         Claims claims = jwtUtils.getClaims(token);
-        System.out.println("claims = " + claims);
+        assertThat(claims.get("userId")).isEqualTo(10);
     }
 
 }
