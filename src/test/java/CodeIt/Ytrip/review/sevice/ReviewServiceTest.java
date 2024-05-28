46e374da738f821f28847486984bee15757728c6
WooJJam <jaemin5548@naver.com>
2024-05-28 20:24:12 +0900

    :white_check_mark: 댓글 생생 테스트 생성, jwt 테스트 수정

---
+++ src/test/java/CodeIt/Ytrip/review/sevice/ReviewServiceTest.java
@@ -0,0 +1,42 @@
+package CodeIt.Ytrip.review.sevice;
+
+import CodeIt.Ytrip.review.domain.Review;
+import CodeIt.Ytrip.user.domain.User;
+import CodeIt.Ytrip.video.domain.Video;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+@SpringBootTest
+@Transactional
+class ReviewServiceTest {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @Test
+    public void 댓글_생성() throws Exception {
+
+        // given
+        User user = new User();
+        user.createUser("테스터","test@test.com","1234",null);
+        em.persist(user);
+
+        Video video = new Video("Test Video Title", "Test Video Content", "url", 10, "Test Video Tag");
+        em.persist(video);
+
+        Review review = new Review(user, video, "Test Review Content");
+        em.persist(review);
+
+        //when
+        Review findReview = em.find(Review.class, review.getId());
+
+        //then
+        Assertions.assertThat(findReview).isEqualTo(review);
+    }
+
+
+}
