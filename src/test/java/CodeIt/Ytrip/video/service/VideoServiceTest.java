269fb97ba93a041cfbbd44cada0744048b04f163
WooJJam <jaemin5548@naver.com>
2024-05-29 01:57:04 +0900

    :white_check_mark: 비디오 상세 정보 조회 테스트

--- src/test/java/CodeIt/Ytrip/video/service/VideoServiceTest.java
+++ src/test/java/CodeIt/Ytrip/video/service/VideoServiceTest.java
@@ -1,20 +1,28 @@
 package CodeIt.Ytrip.video.service;
 
 import CodeIt.Ytrip.video.domain.Video;
-import CodeIt.Ytrip.video.dto.VideoDto;
 import CodeIt.Ytrip.video.repository.VideoRepository;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @SpringBootTest
+@Transactional
 class VideoServiceTest {
 
     @Autowired
     VideoRepository videoRepository;
+
+    @PersistenceContext
+    EntityManager em;
+
     @Test
     public void 비디오_목록_조회() throws Exception {
         //given
@@ -22,7 +30,23 @@ class VideoServiceTest {
         //when
         int size = findVidoes.size();
         //then
-        Assertions.assertThat(size).isEqualTo(5);
+        Assertions.assertThat(size).isEqualTo(12);
+    }
+
+    @Test
+    public void 비디오_상세_정보_조회() throws Exception {
+        Video test = Video.builder()
+                .title("Test Title")
+                .content("Test content")
+                .build();
+
+        videoRepository.save(test);
+
+        Optional<Video> findVideo = videoRepository.findById(test.getId());
+        Assertions.assertThat(test.getId()).isEqualTo(findVideo.get().getId());
+        Assertions.assertThat(test.getTitle()).isEqualTo(findVideo.get().getTitle());
+        Assertions.assertThat(test.getContent()).isEqualTo(findVideo.get().getContent());
+
     }
 
 }
